c9d467a65c980045310e0064058252d88703d895
Niall <noccer@yahoo.ie>
2016-07-13 09:34:24 +1000

    Added a pleasantry and a way to obtain the customers name

--- lunch_orders.rb
+++ lunch_orders.rb
@@ -1,3 +1,8 @@
 # greet the person
 puts "Welcome to Lunch Orders!"
 puts "What is your name?"
+
+#obtain their name
+name = gets.chomp
+# pleasantry
+puts "What would you like to order " + name + "?"
