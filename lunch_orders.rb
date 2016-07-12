e88909969b1ae7df66e9f5d07f115914ab56cf60
Niall <noccer@yahoo.ie>
2016-07-13 09:50:43 +1000

    Added in some logic to check if the person wants to order again. Changed the response checking to boolean instead of y/n. Added in some logic to finish the order if the person doesnt answer y or n.

--- lunch_orders.rb
+++ lunch_orders.rb
@@ -9,16 +9,28 @@ puts "What would you like to order " + name + "?"
 
 # default values
 order = []
-answer = "y"
+answer = true
 
 # taking the orders
-while (answer == "y") do
+while (answer == true) do
   food = gets.chomp
   order.push(food)
   puts "Would you like to order anything else " + name + "? (y/n)"
-  answer = gets.chomp.downcase
+  response = gets.chomp.downcase
+  # convert the answer to a boolean
+  if (response == "y")
+    answer = true
+  elsif (response == "n")
+    # finish ordering because the person either entered "n"
+    answer = false
+  else
+    # the person didnt answer y or n, so we give them some feedback.
+    puts "I'm sorry, I needed you to answer 'y' or 'n'."
+    puts "I'll place the rest of your order now."
+    answer = false
+  end
   # if person opts to place another order, ask them what they want
-  if answer == "y"
+  if answer == true
     puts "What else would you like to order?"
   end
 end
@@ -31,4 +43,4 @@ end
 
 # tell the person their order will be ready in x minutes.
 x = rand(5..10)
-puts "The order will be ready in " + x.to_s + " minutes."
+puts "Your order will be ready in " + x.to_s + " minutes."
