410fbe14a008460576d06f3cf5205272f750f72f
Niall <noccer@yahoo.ie>
2016-07-13 09:35:59 +1000

    Take the persons order and check if they want to continue ordering.

--- lunch_orders.rb
+++ lunch_orders.rb
@@ -10,3 +10,11 @@ puts "What would you like to order " + name + "?"
 # default values
 order = []
 answer = "y"
+
+# taking the orders
+while (answer == "y") do
+  food = gets.chomp
+  order.push(food)
+  puts "Would you like to order anything else " + name + "? (y/n)"
+  answer = gets.chomp.downcase
+end
